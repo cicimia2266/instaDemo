297e1fe990ac4978595cbdab91775f6dcf07c2ff
duanzhang <zhangduan.dtd@gmail.com>
2020-07-19 20:34:44 -0700

    Almost finished project with add post needs update

--- Insta/admin.py
+++ Insta/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from Insta.models import Post, InstaUser, UserConnection, Like, Comment
 
 # Register your models here.
+admin.site.register(Post)
+admin.site.register(InstaUser)
+admin.site.register(UserConnection)
+admin.site.register(Like)
+admin.site.register(Comment)
--- Insta/urls.py
+++ Insta/urls.py
@@ -15,9 +15,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from Insta.views import HelloWorld
+from Insta.views import (HelloWorld, PostsView, PostDetailView, PostCreateView, 
+                            PostUpdateView, PostDeleteView, UserProfile, EditProfile,
+                            toggleFollow, addLike, addComment, ExploreView)
 
 urlpatterns = [
-    path('', HelloWorld.as_view(), name='helloworld')
-
+    path('', HelloWorld.as_view(), name='helloworld'),
+    path('posts/', PostsView.as_view(), name='posts'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', PostCreateView.as_view(), name = 'make_post'),
+    path('post/update/<int:pk>/', PostUpdateView.as_view(), name = 'update_post'),
+    path('post/delete/<int:pk>/', PostDeleteView.as_view(), name = 'delete_post'),
+    path('user_profile/<int:pk>/', UserProfile.as_view(), name='profile'),
+    path('edit_profile/<int:pk>/', EditProfile.as_view(), name='edit_profile'),
+    path('togglefollow', toggleFollow, name='togglefollow'),
+    path('like', addLike, name='addLike'),
+    path('comment', addComment, name='addComment'),
+    path('explore', ExploreView.as_view(), name='explore'),
 ]
